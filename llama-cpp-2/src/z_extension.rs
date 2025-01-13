c2fcef91e3e8b016c061644d20b7f403dfeb706c
Ting Lou <ting.lou@gmail.com>
2025-01-13 17:31:33 +0800

    ext

--- llama-cpp-2/src/z_extension.rs
+++ llama-cpp-2/src/z_extension.rs
@@ -1,4 +1,6 @@
 use crate::context::LlamaContext;
+use crate::model::LlamaModel;
+use crate::sampling::LlamaSampler;
 
 /// exgtend LlamaContext
 impl<'model> LlamaContext<'model> {
@@ -10,5 +12,29 @@ impl<'model> LlamaContext<'model> {
     /// return raw mut pointer
     pub fn as_mut(&mut self) -> *mut llama_cpp_sys_2::llama_context {
         unsafe { self.context.as_mut() }
+    }
+}
+
+impl LlamaModel {
+    /// return raw const pointer
+    pub fn as_ptr(&self) -> *const llama_cpp_sys_2::llama_model {
+        self.model.as_ptr()
+    }
+
+    /// return raw mut pointer
+    pub fn as_mut(&mut self) -> *mut llama_cpp_sys_2::llama_model {
+        unsafe { self.model.as_mut() }
+    }
+}
+
+impl LlamaSampler {
+    /// return raw const pointer
+    pub fn as_ptr(&self) -> *const llama_cpp_sys_2::llama_sampler {
+        self.sampler
+    }
+
+    /// return raw mut pointer
+    pub fn as_mut(&mut self) -> *mut llama_cpp_sys_2::llama_sampler {
+        self.sampler
     }
 }
